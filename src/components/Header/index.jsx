d3bbd0eb90ff8489d04e06e68072a7b7dc294556
Ana Santos <ana.br@hotmail.com>
2024-06-04 09:01:45 -0300

    ajustes na responsividade do header

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,13 +1,13 @@
-"use client"
-import React, { useState } from 'react';
-import Image from 'next/image';
-import Link from 'next/link';
-import { useMediaQuery } from 'react-responsive';
+"use client";
+import React, { useState } from "react";
+import Image from "next/image";
+import Link from "next/link";
+import { useMediaQuery } from "react-responsive";
 
 const Header = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
   const isMobile = useMediaQuery({ maxWidth: 768 });
-  const [searchTerm, setSearchTerm] = useState('');
+  const [searchTerm, setSearchTerm] = useState("");
 
   const toggleMenu = () => {
     setIsMenuOpen(!isMenuOpen);
@@ -24,44 +24,72 @@ const Header = () => {
 
   return (
     <div className="bg-bgHeader bg-footer bg-cover h-24 flex items-center relative">
-      <div className="w-full flex justify-between items-center px-4">
+      <div className="w-full flex justify-between items-center px-4 md:px-8 lg:px-16">
         <div className="flex-shrink-0">
-          <Image src="/logoHeader.png" alt="Logo" width={99} height={76} priority={true} />
+          <Image
+            src="/logoHeader.png"
+            alt="Logo"
+            width={99}
+            height={76}
+            priority={true}
+          />
         </div>
-        <nav className={`flex-grow justify-center space-x-11 ${isMobile ? 'hidden' : 'flex'} md:flex`}>
+        <nav
+          className={`${
+            isMobile ? "hidden" : "flex"
+          } flex-grow justify-center space-x-2 sm:space-x-4 md:space-x-6 lg:space-x-8 xl:space-x-10 2xl:space-x-12 items-center`}
+        >
           <Link href="/">
-            <p className="text-white hover:text-gray-200 px-4 py-2 cursor-pointer text-shadow font-be-vietnam text-15px font-500">Página Inicial</p>
+            <p className="text-white hover:text-gray-200 px-2 md:px-4 py-2 cursor-pointer text-shadow font-be-vietnam text-15px font-500">
+              Página Inicial
+            </p>
           </Link>
           <Link href="/hepatohub">
-            <p className="text-white hover:text-gray-200 px-4 py-2 cursor-pointer text-shadow font-be-vietnam text-15px font-500">Sobre</p>
+            <p className="text-white hover:text-gray-200 px-2 md:px-4 py-2 cursor-pointer text-shadow font-be-vietnam text-15px font-500">
+              Sobre
+            </p>
           </Link>
           <Link href="/autocuidado">
-            <p className="text-white hover:text-gray-200 px-4 py-2 cursor-pointer text-shadow font-be-vietnam text-15px font-500">Auto Cuidado</p>
+            <p className="text-white hover:text-gray-200 px-2 md:px-4 py-2 cursor-pointer text-shadow font-be-vietnam text-15px font-500">
+              Auto Cuidado
+            </p>
           </Link>
           <Link href="/construcao">
-            <p className="text-white hover:text-gray-200 px-4 py-2 cursor-pointer text-shadow font-be-vietnam text-15px font-500">Medicamentos</p>
+            <p className="text-white hover:text-gray-200 px-2 md:px-4 py-2 cursor-pointer text-shadow font-be-vietnam text-15px font-500">
+              Medicamentos
+            </p>
           </Link>
           <Link href="/construcao">
-            <p className="text-white hover:text-gray-200 px-4 py-2 cursor-pointer text-shadow font-be-vietnam text-15px font-500">Conheça a Equipe</p>
+            <p className="text-white hover:text-gray-200 px-2 md:px-4 py-2 cursor-pointer text-shadow font-be-vietnam text-15px font-500">
+              Conheça a Equipe
+            </p>
           </Link>
           <Link href="/construcao">
-            <p className="text-white hover:text-gray-200 px-4 py-2 cursor-pointer text-shadow font-be-vietnam text-15px font-500">Colabore</p>
+            <p className="text-white hover:text-gray-200 px-2 md:px-4 py-2 cursor-pointer text-shadow font-be-vietnam text-15px font-500">
+              Colabore
+            </p>
           </Link>
         </nav>
         <div className="ml-auto flex-shrink-0 relative">
           <div className="hidden md:flex items-center">
             <form onSubmit={handleSearch} className="relative">
-              <input 
-                type="text" 
-                placeholder="Pesquisar..." 
-                value={searchTerm} 
-                onChange={handleChange} 
-                className="bg-white text-gray-800 px-4 py-2 rounded-full focus:outline-none focus:ring-2 focus:ring-blue-400" 
+              <input
+                type="text"
+                placeholder="Pesquisar..."
+                value={searchTerm}
+                onChange={handleChange}
+                className="bg-white text-gray-800 px-4 py-2 rounded-full focus:outline-none focus:ring-2 focus:ring-blue-400"
               />
               <div className="absolute right-2 top-1/2 transform -translate-y-1/2">
-                <div className='bg-footer rounded-full flex justify-center items-center w-[22px] h-[23px]'>
+                <div className="bg-footer rounded-full flex justify-center items-center w-[22px] h-[23px]">
                   <button type="submit">
-                    <Image src="/lupa.png" alt="Ícone de busca" width={14} height={14} priority={true} />
+                    <Image
+                      src="/lupa.png"
+                      alt="Ícone de busca"
+                      width={14}
+                      height={14}
+                      priority={true}
+                    />
                   </button>
                 </div>
               </div>
@@ -69,8 +97,19 @@ const Header = () => {
           </div>
           <div className="md:hidden flex items-center">
             <button onClick={toggleMenu} className="text-white focus:outline-none">
-              <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16m-7 6h7"></path>
+              <svg
+                className="w-6 h-6"
+                fill="none"
+                stroke="currentColor"
+                viewBox="0 0 24 24"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M4 6h16M4 12h16m-7 6h7"
+                ></path>
               </svg>
             </button>
           </div>
@@ -79,35 +118,53 @@ const Header = () => {
       {isMobile && isMenuOpen && (
         <div className="absolute top-full left-0 right-0 bg-h1-color bg-cover mt-2 px-4 space-y-2">
           <Link href="/">
-            <p className="text-white hover:text-gray-200 px-4 py-2 cursor-pointer text-shadow font-be-vietnam text-15px font-500">Página Inicial</p>
+            <p className="text-white hover:text-gray-200 px-4 py-2 cursor-pointer text-shadow font-be-vietnam text-15px font-500">
+              Página Inicial
+            </p>
           </Link>
           <Link href="/hepatohub">
-            <p className="text-white hover:text-gray-200 px-4 py-2 cursor-pointer text-shadow font-be-vietnam text-15px font-500">Sobre</p>
+            <p className="text-white hover:text-gray-200 px-4 py-2 cursor-pointer text-shadow font-be-vietnam text-15px font-500">
+              Sobre
+            </p>
           </Link>
           <Link href="/autocuidado">
-            <p className="text-white hover:text-gray-200 px-4 py-2 cursor-pointer text-shadow font-be-vietnam text-15px font-500">Auto Cuidado</p>
+            <p className="text-white hover:text-gray-200 px-4 py-2 cursor-pointer text-shadow font-be-vietnam text-15px font-500">
+              Auto Cuidado
+            </p>
           </Link>
           <Link href="/construcao">
-            <p className="text-white hover:text-gray-200 px-4 py-2 cursor-pointer text-shadow font-be-vietnam text-15px font-500">Medicamentos</p>
+            <p className="text-white hover:text-gray-200 px-4 py-2 cursor-pointer text-shadow font-be-vietnam text-15px font-500">
+              Medicamentos
+            </p>
           </Link>
           <Link href="/construcao">
-            <p className="text-white hover:text-gray-200 px-4 py-2 cursor-pointer text-shadow font-be-vietnam text-15px font-500">Conheça a Equipe</p>
+            <p className="text-white hover:text-gray-200 px-4 py-2 cursor-pointer text-shadow font-be-vietnam text-15px font-500">
+              Conheça a Equipe
+            </p>
           </Link>
           <Link href="/construcao">
-            <p className="text-white hover:text-gray-200 px-4 py-2 cursor-pointer text-shadow font-be-vietnam text-15px font-500">Colabore</p>
+            <p className="text-white hover:text-gray-200 px-4 py-2 cursor-pointer text-shadow font-be-vietnam text-15px font-500">
+              Colabore
+            </p>
           </Link>
           <form onSubmit={handleSearch} className="relative mt-2">
-            <input 
-              type="text" 
-              placeholder="Pesquisar..." 
-              value={searchTerm} 
-              onChange={handleChange} 
-              className="bg-white text-gray-800 px-4 py-2 rounded-full focus:outline-none focus:ring-2 focus:ring-blue-400 w-full" 
+            <input
+              type="text"
+              placeholder="Pesquisar..."
+              value={searchTerm}
+              onChange={handleChange}
+              className="bg-white text-gray-800 px-4 py-2 rounded-full focus:outline-none focus:ring-2 focus:ring-blue-400 w-full"
             />
             <div className="absolute right-2 top-1/2 transform -translate-y-1/2">
-              <div className='bg-footer rounded-full flex justify-center items-center w-[22px] h-[23px]'>
+              <div className="bg-footer rounded-full flex justify-center items-center w-[22px] h-[23px]">
                 <button type="submit">
-                  <Image src="/lupa.png" alt="Ícone de busca" width={14} height={14} priority={true} />
+                  <Image
+                    src="/lupa.png"
+                    alt="Ícone de busca"
+                    width={14}
+                    height={14}
+                    priority={true}
+                  />
                 </button>
               </div>
             </div>
